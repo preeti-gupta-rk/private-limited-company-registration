ff0003f3ebd045d66543369ad20c9485d3ef4bd8
preeti-gupta-rk <preetigupta@registerkaro.com>
2025-03-20 10:54:16 +0530

    Update CostPenalities.tsx

--- private-limited-company-registration-in-state/CostPenalities.tsx
+++ private-limited-company-registration-in-state/CostPenalities.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
 interface CostPenalitiesProps{
+  state:string,
   penaltiesContent:{
     para:string
+    para1:string
+    para2:string
   }
 }
 
 const CostPenalities = (props:CostPenalitiesProps) => {
-  const {para} = props.penaltiesContent;
+  const {para, para1,para2} = props.penaltiesContent;
+  const {state} = props;
   return (
     <div className='cost-penalities-container' id="cost-and-penalties" style={{scrollMarginTop:"130px"}}>
-      <h2 className='heading-cost-penalities'>Cost and Penalties</h2>
-      <p>content needed to added</p>
+      <h2 className='heading-cost-penalities' style={{"fontWeight":700, color:"#000",}}> <span style={{color:"#ffa229",}}>Cost and Penalties</span> of Registration in {state}</h2>
       <p>{para}</p>
+      <p>{para1}</p>
+      <p>{para2}</p>
     </div>
   )
 }
