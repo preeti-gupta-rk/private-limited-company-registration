32deffe1ebc3d084c863f041e2abae590ad1e9d2
preeti-gupta-rk <preetigupta@registerkaro.com>
2025-03-20 10:55:01 +0530

    Update CompanyRegistrationAuthorization.tsx

--- private-limited-company-registration-in-state/CompanyRegistrationAuthorization.tsx
+++ private-limited-company-registration-in-state/CompanyRegistrationAuthorization.tsx
@@ -3,16 +3,17 @@ interface companyRegistrationAuthorizationProps{
   state:string;
   authorizationContent:{
     para1?:string;
+    para2?: string
   }
 }
 const CompanyRegistrationAuthorization = (props:companyRegistrationAuthorizationProps) => {
-  const {state, authorizationContent:{para1}} = props;
+  const {state, authorizationContent:{para1,para2}} = props;
 
   return (
     <div className='authorization-container' id="company-registration-authorisation" style={{scrollMarginTop:"135px"}}>
-     <h2> <span>Company Registration Authorization</span> in {state} </h2>
-     <p>content to be added</p>
+     <h2 className='heading-company-registration-authorization' style={{"fontWeight":700, color:"#000",}}> <span style={{color:"#ffa229",}}>Company Registration Authorization</span> in {state} </h2>
      <p style={{textAlign:"justify"}}>{para1}</p>
+     <p style={{textAlign:"justify"}}>{para2}</p>
     </div>
   )
 }
